e5e53660318f5c0484bd1d848704db6a477a5d92
matt.h <matt.huang@sjfood.com>
2026-05-01 10:18:17 +0800

    Fix Vercel build: add serverComponentsExternalPackages config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
+  // 确保 API 路由正确处理
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
 }
 
 module.exports = nextConfig
